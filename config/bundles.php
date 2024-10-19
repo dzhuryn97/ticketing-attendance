4504fb16957f12ae2f509eb90fe34d2f71bd437a
Volodymyr Dzhuryn <volodymyr.dzhuryn@otakoyi.com>
2024-10-19 15:31:58 +0300

    try first ci

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
